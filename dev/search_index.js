226dd1346be48efabb0638cd22e2f097b6fb38a4
Documenter.jl <documenter@juliadocs.github.io>
2022-06-24 08:06:49 +0000

    build based on e5cffff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Kaneko","category":"page"},{"location":"#Kaneko","page":"Home","title":"Kaneko","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Kaneko.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Kaneko]","category":"page"}]
+}
